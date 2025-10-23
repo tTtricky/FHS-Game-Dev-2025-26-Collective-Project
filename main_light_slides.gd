383c698fa34ca2ca38211abe0768b0388c5a123b
Myclfi <mmicha.shapiro@gmail.com>
2025-10-23 06:49:45 -0600

    Finished Mainlight Position and Rotation Functionality

--- main_light_slides.gd
+++ main_light_slides.gd
@@ -4,4 +4,11 @@ class_name light
 func _process(delta: float) -> void:
 	$Stagelight.rotation = $Sliders/Rotation.value / 150
 	$Stagelight.position.x = $Sliders/Position.value
-	
+	if Input.is_action_pressed("Left"):
+		$Sliders/Position.value -= 2
+	if Input.is_action_pressed("Right"):
+		$Sliders/Position.value += 2
+	if Input.is_action_pressed("A"):
+		$Sliders/Rotation.value -= 2
+	if Input.is_action_pressed("D"):
+		$Sliders/Rotation.value += 2
